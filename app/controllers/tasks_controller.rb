449b88c6ab5ef98183f82198d6814a1b8d0e9169
Nuria Toledano <ia.andyouare@gmail.com>
2022-04-23 17:06:14 +0100

    Added remaining CRUD actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,8 +9,44 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
   # Create a task
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  # Edit a task
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   # Update a task
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
   # Destroy a task
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 
 end
